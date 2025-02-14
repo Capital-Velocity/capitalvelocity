a23ee9669ff23aa9a310580c71e84cb321909aaf
Jason Lui <luijason1221@gmail.com>
2025-02-14 08:59:34 -0800

    fix horizontal bleed spacing on mobile

--- frontend/src/components/Loan Form/LoanTerms3.jsx
+++ frontend/src/components/Loan Form/LoanTerms3.jsx
@@ -12,45 +12,10 @@ import { Divider } from "@mui/material";
 
 function LoanTerms3({ formData, setFormData }) {
   const [interestRate, setInterestRate] = useState(10.74);
-  const [selectedOption, setSelectedOption] = useState("percentage");
-
-  const handleRadioChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
-
-  const [selectedMonth, setSelectedMonth] = useState("");
-
-  const handleMonthChange = (event) => {
-    setSelectedMonth(event.target.value);
-  };
-
-  const renderMenuItems = () => {
-    const months = Array.from({ length: 18 }, (_, index) => index + 1);
-
-    return months.map((month) => (
-      <MenuItem key={month} value={month}>
-        {month} {month === 1 ? "Month" : "Months"}
-      </MenuItem>
-    ));
-  };
-
-  const getInputAdornment = () => {
-    return selectedOption === "percentage" ? "%" : "$";
-  };
-
-  const handleInterestRateChange = (event) => {
-    setInterestRate(parseFloat(event.target.value));
-  };
-
-  const interestRates = [];
-  for (let rate = 10.74; rate <= 16; rate += 0.5) {
-    interestRates.push(rate.toFixed(2));
-  }
 
   return (
     <div style={{ width: "100%" }}>
-      {" "}
-      <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
+      <CheckoutSteps step1 step2 step3 step4 />
       <Container>
         <Typography variant="h4" color="black" gutterBottom>
           Vendor and Closing Options
@@ -58,135 +23,100 @@ function LoanTerms3({ formData, setFormData }) {
         <Divider style={{ color: "grey", marginBottom: 10 }} />
 
         <Grid container spacing={2}>
-          <Grid item sm={6}>
-            {/* This is for the textFields*/}
-            <Grid container spacing={5}>
-              <Grid item sm={12}>
-                <FormControl fullWidth>
-                  <InputLabel
-                    style={{ fontSize: 15, fontWeight: 100 }}
-                    id="demo-simple-select-label"
-                  >
-                    Preferred Closing Attorney *
-                  </InputLabel>
-                  <Select
-                    style={{ width: "500px", backgroundColor: "white" }}
-                    InputLabelProps={{
-                      style: { fontSize: 15, fontWeight: 100 },
-                    }}
-                    value={formData.preferredClosingAttorney || ""}
-                    onChange={(e) =>
-                      setFormData({
-                        ...formData,
-                        preferredClosingAttorney: e.target.value,
-                      })
-                    }
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Entity Type"
-                  >
-                    <MenuItem value={"Hartmann Doherty Rosa Berman & Bulbulia"}>
-                      Hartmann Doherty Rosa Berman & Bulbulia
-                    </MenuItem>
-                    <MenuItem value={"REO"}>
-                      LaRocca Hornik Rosen & Greenberg LLP
-                    </MenuItem>
-                    <MenuItem value={"Geraci Law Firm"}>
-                      Geraci Law Firm
-                    </MenuItem>
-                    <MenuItem value={"Foreclosure Auction"}>
-                      Geraci Law Firm
-                    </MenuItem>
-                    <MenuItem value={"Law Office of Lawrence Andelsman"}>
-                      Law Office of Lawrence Andelsman
-                    </MenuItem>
-                    <MenuItem value={"Deutsch & Schneider LLP"}>
-                      Deutsch & Schneider LLP
-                    </MenuItem>
-                    <MenuItem value={"Kaplan Kaplan & Ditrapani, LLP"}>
-                      Kaplan Kaplan & Ditrapani, LLP
-                    </MenuItem>
-                    <MenuItem value={"Mangelli & Bilotti PLLC"}>
-                      Mangelli & Bilotti PLLC
-                    </MenuItem>
-                    <MenuItem value={"MLS"}>Michele R.Messian P.C.</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item sm={12}>
-                <FormControl fullWidth>
-                  <Typography type="p" color="grey">
-                    Preferred Closing Date
-                  </Typography>
-                  <Grid item sm={12}>
-                    {" "}
-                    <TextField
-                      value={formData.closingDate || ""}
-                      onChange={(e) =>
-                        setFormData({
-                          ...formData,
-                          closingDate: e.target.value,
-                        })
-                      }
-                      style={{ width: 500, backgroundColor: "white" }}
-                      type="date"
-                      variant="outlined"
-                      fullWidth
-
-                      // Add more props as needed
-                    />
-                  </Grid>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item sm={6}>
-            <Grid item sm={12}>
-              <FormControl fullWidth>
-                <InputLabel
-                  style={{ fontSize: 15, fontWeight: 100 }}
-                  id="demo-simple-select-label"
-                >
-                  Preferred Insurance Company *
-                </InputLabel>
-                <Select
-                  style={{ width: "500px", backgroundColor: "white" }}
-                  InputLabelProps={{
-                    style: { fontSize: 15, fontWeight: 100 },
-                  }}
-                  value={formData.insuranceCompany || ""}
-                  onChange={(e) =>
-                    setFormData({
-                      ...formData,
-                      insuranceCompany: e.target.value,
-                    })
-                  }
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Entity Type"
-                >
-                  <MenuItem value={"Elmsure"}>Elmsure</MenuItem>
-                  <MenuItem value={"Other"}>Other</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <FormControl fullWidth style={{ marginTop: 32 }}>
-              <InputLabel
-                style={{ fontSize: 15, fontWeight: 100 }}
-                id="demo-simple-select-label"
+          {/* Preferred Closing Attorney */}
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="closing-attorney-label">
+                Preferred Closing Attorney *
+              </InputLabel>
+              <Select
+                labelId="closing-attorney-label"
+                value={formData.preferredClosingAttorney || ""}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    preferredClosingAttorney: e.target.value,
+                  })
+                }
+                sx={{ backgroundColor: "white" }}
               >
+                <MenuItem value={"Hartmann Doherty Rosa Berman & Bulbulia"}>
+                  Hartmann Doherty Rosa Berman & Bulbulia
+                </MenuItem>
+                <MenuItem value={"REO"}>
+                  LaRocca Hornik Rosen & Greenberg LLP
+                </MenuItem>
+                <MenuItem value={"Geraci Law Firm"}>Geraci Law Firm</MenuItem>
+                <MenuItem value={"Foreclosure Auction"}>
+                  Geraci Law Firm
+                </MenuItem>
+                <MenuItem value={"Law Office of Lawrence Andelsman"}>
+                  Law Office of Lawrence Andelsman
+                </MenuItem>
+                <MenuItem value={"Deutsch & Schneider LLP"}>
+                  Deutsch & Schneider LLP
+                </MenuItem>
+                <MenuItem value={"Kaplan Kaplan & Ditrapani, LLP"}>
+                  Kaplan Kaplan & Ditrapani, LLP
+                </MenuItem>
+                <MenuItem value={"Mangelli & Bilotti PLLC"}>
+                  Mangelli & Bilotti PLLC
+                </MenuItem>
+                <MenuItem value={"MLS"}>Michele R. Messian P.C.</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          {/* Preferred Closing Date */}
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <Typography color="grey">Preferred Closing Date</Typography>
+              <TextField
+                value={formData.closingDate || ""}
+                onChange={(e) =>
+                  setFormData({ ...formData, closingDate: e.target.value })
+                }
+                type="date"
+                variant="outlined"
+                fullWidth
+                sx={{ backgroundColor: "white" }}
+              />
+            </FormControl>
+          </Grid>
+
+          {/* Preferred Insurance Company */}
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="insurance-company-label">
+                Preferred Insurance Company *
+              </InputLabel>
+              <Select
+                labelId="insurance-company-label"
+                value={formData.insuranceCompany || ""}
+                onChange={(e) =>
+                  setFormData({ ...formData, insuranceCompany: e.target.value })
+                }
+                sx={{ backgroundColor: "white" }}
+              >
+                <MenuItem value={"Elmsure"}>Elmsure</MenuItem>
+                <MenuItem value={"Other"}>Other</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          {/* Preferred Title Company */}
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth sx={{ mt: { xs: 2, sm: 4 } }}>
+              <InputLabel id="title-company-label">
                 Preferred Title Company
               </InputLabel>
               <Select
-                style={{ width: "500px", backgroundColor: "white" }}
-                InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
+                labelId="title-company-label"
                 value={formData.titleCompany || ""}
                 onChange={(e) =>
                   setFormData({ ...formData, titleCompany: e.target.value })
                 }
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Entity Type"
+                sx={{ backgroundColor: "white" }}
               >
                 <MenuItem value={"None"}>None</MenuItem>
                 <MenuItem value={"Other"}>Other</MenuItem>
