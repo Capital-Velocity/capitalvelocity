01dc8e379b146efaf9243734f2af5d67772c0c61
Jason Lui <luijason1221@gmail.com>
2025-02-14 09:25:18 -0800

    change content with fix and flip calculator

--- frontend/src/components/Loan Form/LoanTerms.jsx
+++ frontend/src/components/Loan Form/LoanTerms.jsx
@@ -8,6 +8,7 @@ import React, { useState } from "react";
 import Container from "../../screens/Container";
 import CheckoutSteps from "../CheckoutSteps";
 import { Divider } from "@mui/material";
+import FixandFlipCalc from "../../Calculators/FixandFlipCalc";
 
 function LoanTerms({ formData, setFormData, fieldErrors }) {
   const [sliderValue, setSliderValue] = React.useState(1);
@@ -67,116 +68,18 @@ function LoanTerms({ formData, setFormData, fieldErrors }) {
       {" "}
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
       <Container>
-        <Typography variant="h4" color="black" gutterBottom>
+        {/* <Typography variant="h4" color="black" gutterBottom>
           Loan Terms
         </Typography>
         <Typography variant="subtitle1" color="grey" gutterBottom>
           Let's do some math here with the loan terms and pricing
-        </Typography>
+        </Typography> */}
         <Divider style={{ color: "grey", marginBottom: 10 }} />
 
         <Grid container spacing={2}>
-          <Grid item sm={8}>
+          <Grid item sm={12}>
             {/* This is for the textFields*/}
-            <Grid container spacing={5}>
-              <Grid item sm={6}>
-                <FormControl fullWidth>
-                  <Typography type="p" color="grey">
-                    Initial Loan Amount
-                  </Typography>
-                  <TextField
-                    type="number"
-                    error={fieldErrors.initialLoanAmount}
-                    helperText={<span>{fieldErrors.initialLoanAmount}</span>}
-                    value={formData.initialLoanAmount || initialLoanAmount}
-                    onChange={(event) => handleChangeLoan(event)}
-                    style={{ marginTop: 10, backgroundColor: "white" }}
-                    fullWidth
-                    variant="outlined"
-                    InputProps={{
-                      startAdornment: <Typography variant="h6">$</Typography>,
-                    }}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item sm={6}>
-                <Typography variant="subtitle1" color="grey" gutterBottom>
-                  Initial Loan Amount/Purchase Price
-                </Typography>
-                <div style={{ width: "300px" }}>
-                  <Slider
-                    value={formData.purchaseAmount || ""}
-                    onChange={(event) => setLoanPercentage(event)}
-                    valueLabelDisplay="auto"
-                    min={0}
-                    max={75}
-                    marks={percentageMarks}
-                    color="success"
-                    style={{ color: "#498dd6" }}
-                    aria-labelledby="loan-percentage-slider"
-                  />
-                </div>
-              </Grid>
-              <Grid item sm={6}>
-                <FormControl fullWidth>
-                  <Typography type="p" color="grey">
-                    Construction Holdback
-                  </Typography>
-                  <TextField
-                    style={{ backgroundColor: "white" }}
-                    type="number"
-                    value={formData.constructionHoldback || ""}
-                    error={fieldErrors.constructionHoldback}
-                    helperText={<span>{fieldErrors.constructionHoldback}</span>}
-                    onChange={(event) =>
-                      handleChangeLoanConstructionHoldback(event)
-                    }
-                    fullWidth
-                    variant="outlined"
-                    InputProps={{
-                      startAdornment: <Typography variant="h6">$</Typography>,
-                    }}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item sm={6}>
-                <Typography variant="subtitle1" color="grey" gutterBottom>
-                  Construction Holdback/Renovation
-                </Typography>
-                <div style={{ width: "300px" }}>
-                  <Slider
-                    value={formData.constructionBudgetPercentage || ""}
-                    onChange={(event) => setConstruction(event)}
-                    valueLabelDisplay="auto"
-                    min={0}
-                    max={100}
-                    marks={percentageMarks2}
-                    color="success"
-                    style={{ color: "#498dd6" }}
-                    aria-labelledby="construction-budget-percentage-slider"
-                  />
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item sm={4}>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginTop: 2,
-                borderRadius: 10,
-                width: "100%",
-                height: 60,
-                backgroundColor: "white",
-                border: "2px solid #498dd6",
-                color: "black",
-                fontSize: 24,
-              }}
-            >
-              Total: ${calculateTotal().toFixed(2)}
-            </Box>
+            <FixandFlipCalc />
           </Grid>
         </Grid>
       </Container>
