a953a71e2e7db5e26846346e933c1da7fef3a7fe
Jason Lui <luijason1221@gmail.com>
2025-02-18 09:36:33 -0800

    center elements and fix inconsistent font

--- frontend/src/components/Loan Form/EntityInformation.jsx
+++ frontend/src/components/Loan Form/EntityInformation.jsx
@@ -44,7 +44,7 @@ function EntityInformation({ formData, setFormData, fieldErrors }) {
         </Typography>
         <Divider style={{ color: "black", marginBottom: 10 }} />
 
-        <Grid item xs={12} sx={{ mt: 2, textAlign: "center" }}>
+        <Grid item xs={12} sx={{ textAlign: "center" }}>
           <FormControl
             component="fieldset"
             error={Boolean(fieldErrors?.guranteeLoan)}
@@ -88,157 +88,165 @@ function EntityInformation({ formData, setFormData, fieldErrors }) {
           </FormControl>
         </Grid>
 
-        <Typography type="p" color="black">
-          Do you have the Borrowing Entity Information?
-        </Typography>
-        <RadioGroup
-          row
-          aria-labelledby="demo-row-radio-buttons-group-label"
-          name="row-radio-buttons-group"
-          style={{ justifyContent: "center" }}
-        >
-          <FormControlLabel
-            style={{ color: "black" }}
-            value="Yes" // Set the value to "Yes" when selected
-            checked={formData.borrowingEntityInformation === "Yes"} // Check if it's "Yes" in formData
-            onChange={() =>
-              handleOptionChange("borrowingEntityInformation", "Yes")
-            }
-            error={fieldErrors.borrowingEntityInformation}
-            helperText={<span>{fieldErrors.borrowingEntityInformation}</span>}
-            control={<Radio />}
-            label="Yes"
-          />
-          <FormControlLabel
-            style={{ color: "black" }}
-            value="No" // Set the value to "Yes" when selected
-            checked={formData.borrowingEntityInformation === "No"} // Check if it's "Yes" in formData
-            onChange={() =>
-              handleOptionChange("borrowingEntityInformation", "No")
-            }
-            error={fieldErrors.borrowingEntityInformation}
-            helperText={<span>{fieldErrors.borrowingEntityInformation}</span>}
-            control={<Radio />}
-            label="No"
-          />
-        </RadioGroup>
-        {fieldErrors.borrowingEntityInformation && (
-          <FormHelperText error style={{ textAlign: "center" }}>
-            {fieldErrors.borrowingEntityInformation}
-          </FormHelperText>
-        )}
+        <Grid item xs={12} sx={{ textAlign: "center" }}>
+          <Typography type="p" color="black">
+            Do you have the Borrowing Entity Information?
+          </Typography>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            style={{ justifyContent: "center" }}
+          >
+            <FormControlLabel
+              style={{ color: "black" }}
+              value="Yes" // Set the value to "Yes" when selected
+              checked={formData.borrowingEntityInformation === "Yes"} // Check if it's "Yes" in formData
+              onChange={() =>
+                handleOptionChange("borrowingEntityInformation", "Yes")
+              }
+              error={fieldErrors.borrowingEntityInformation}
+              helperText={<span>{fieldErrors.borrowingEntityInformation}</span>}
+              control={<Radio />}
+              label="Yes"
+            />
+            <FormControlLabel
+              style={{ color: "black" }}
+              value="No" // Set the value to "Yes" when selected
+              checked={formData.borrowingEntityInformation === "No"} // Check if it's "Yes" in formData
+              onChange={() =>
+                handleOptionChange("borrowingEntityInformation", "No")
+              }
+              error={fieldErrors.borrowingEntityInformation}
+              helperText={<span>{fieldErrors.borrowingEntityInformation}</span>}
+              control={<Radio />}
+              label="No"
+            />
+          </RadioGroup>
+          {fieldErrors.borrowingEntityInformation && (
+            <FormHelperText error style={{ textAlign: "center" }}>
+              {fieldErrors.borrowingEntityInformation}
+            </FormHelperText>
+          )}
 
-        {formData.borrowingEntityInformation === "Yes" && (
-          <Grid container spacing={2}>
-            <Grid item sm={6} xs={12}>
-              <Typography type="p" color="black">
-                Entity Name
-              </Typography>
-              <TextField
-                style={{ width: "100%", backgroundColor: "white" }}
-                value={formData.entityName || ""}
-                size="large"
-                InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
-                onChange={(e) =>
-                  setFormData({ ...formData, entityName: e.target.value })
-                }
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item sm={6} xs={12}>
-              <FormControl fullWidth>
+          {formData.borrowingEntityInformation === "Yes" && (
+            <Grid container spacing={2}>
+              <Grid item sm={6} xs={12}>
                 <Typography type="p" color="black">
-                  Entity Type
+                  Entity Name
                 </Typography>
-                <Select
-                  style={{ backgroundColor: "white" }}
+                <TextField
+                  style={{ width: "100%", backgroundColor: "white" }}
+                  value={formData.entityName || ""}
+                  size="large"
                   InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
-                  value={formData.entityType || ""}
                   onChange={(e) =>
-                    setFormData({ ...formData, entityType: e.target.value })
+                    setFormData({ ...formData, entityName: e.target.value })
                   }
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Entity Type"
-                >
-                  <MenuItem value={"Corperation"}>Corperation</MenuItem>
-                  <MenuItem value={"Limited Liability Company"}>
-                    Limited Liability Company
-                  </MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item sm={6} xs={12}>
-              <Typography type="p" color="black">
-                Date of Incorporation
-              </Typography>
-              <TextField
-                style={{ width: "100%", backgroundColor: "white" }}
-                value={formData.dateofIncorp || ""}
-                InputLabelProps={{
-                  style: {
-                    fontSize: 15,
-                    fontWeight: 100,
-                    backgroundColor: "white",
-                  },
-                }}
-                onChange={(e) =>
-                  setFormData({ ...formData, dateofIncorp: e.target.value })
-                }
-                type="date"
-                id="outlined-basic"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item sm={6} xs={12}>
-              <Typography type="p" color="black">
-                Contact Last Name
-              </Typography>
-              <TextField
-                style={{ width: "100%", backgroundColor: "white" }}
-                value={formData.contactLastName || ""}
-                InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
-                onChange={(e) =>
-                  setFormData({ ...formData, contactLastName: e.target.value })
-                }
-                id="outlined-basic"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item sm={6} xs={12}>
-              <Typography type="p" color="black">
-                Entity Address
-              </Typography>
-              <TextField
-                style={{ width: "100%", backgroundColor: "white" }}
-                InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
-                value={formData.entityAddress || ""}
-                onChange={(e) =>
-                  setFormData({ ...formData, entityAddress: e.target.value })
-                }
-                id="outlined-basic"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12} sx={{}}>
-              <label style={{ fontSize: 15, fontWeight: 100, color: "black" }}>
-                What percentage of the borrowing entity does this borrower own?
-              </label>
-              <div style={{ width: "100%" }}>
-                <Slider
-                  value={formData.borrowingEntityOwned || "0"}
-                  onChange={(event) => handleChange2(event)}
-                  min={0}
-                  max={100}
-                  step={1}
-                  marks={percentageMarks}
-                  valueLabelDisplay="auto"
-                  style={{ color: "#498dd6", width: "100%" }} // Ensure slider takes full width
+                  variant="outlined"
                 />
-              </div>
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <FormControl fullWidth>
+                  <Typography type="p" color="black">
+                    Entity Type
+                  </Typography>
+                  <Select
+                    style={{ backgroundColor: "white" }}
+                    InputLabelProps={{
+                      style: { fontSize: 15, fontWeight: 100 },
+                    }}
+                    value={formData.entityType || ""}
+                    onChange={(e) =>
+                      setFormData({ ...formData, entityType: e.target.value })
+                    }
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Entity Type"
+                  >
+                    <MenuItem value={"Corperation"}>Corperation</MenuItem>
+                    <MenuItem value={"Limited Liability Company"}>
+                      Limited Liability Company
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <Typography type="p" color="black">
+                  Date of Incorporation
+                </Typography>
+                <TextField
+                  style={{ width: "100%", backgroundColor: "white" }}
+                  value={formData.dateofIncorp || ""}
+                  InputLabelProps={{
+                    style: {
+                      fontSize: 15,
+                      fontWeight: 100,
+                      backgroundColor: "white",
+                    },
+                  }}
+                  onChange={(e) =>
+                    setFormData({ ...formData, dateofIncorp: e.target.value })
+                  }
+                  type="date"
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <Typography type="p" color="black">
+                  Contact Last Name
+                </Typography>
+                <TextField
+                  style={{ width: "100%", backgroundColor: "white" }}
+                  value={formData.contactLastName || ""}
+                  InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      contactLastName: e.target.value,
+                    })
+                  }
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <Typography type="p" color="black">
+                  Entity Address
+                </Typography>
+                <TextField
+                  style={{ width: "100%", backgroundColor: "white" }}
+                  InputLabelProps={{ style: { fontSize: 15, fontWeight: 100 } }}
+                  value={formData.entityAddress || ""}
+                  onChange={(e) =>
+                    setFormData({ ...formData, entityAddress: e.target.value })
+                  }
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12} sx={{}}>
+                <Typography type="p" color="black">
+                  What percentage of the borrowing entity does this borrower
+                  own?{" "}
+                </Typography>
+                <div style={{ width: "100%" }}>
+                  <Slider
+                    value={formData.borrowingEntityOwned || "0"}
+                    onChange={(event) => handleChange2(event)}
+                    min={0}
+                    max={100}
+                    step={1}
+                    marks={percentageMarks}
+                    valueLabelDisplay="auto"
+                    style={{ color: "#498dd6", width: "100%" }} // Ensure slider takes full width
+                  />
+                </div>
+              </Grid>
             </Grid>
-          </Grid>
-        )}
+          )}
+        </Grid>
       </Container>
     </div>
   );
