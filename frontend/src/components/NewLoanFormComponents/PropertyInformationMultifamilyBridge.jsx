4e018ced0c948815a33b46dcb44b7713297c1423
Jason Lui <luijason1221@gmail.com>
2025-02-20 14:45:10 -0800

    finalze model/routers on multifamily bridge, fix renovation description

--- frontend/src/components/NewLoanFormComponents/PropertyInformationMultifamilyBridge.jsx
+++ frontend/src/components/NewLoanFormComponents/PropertyInformationMultifamilyBridge.jsx
@@ -438,7 +438,16 @@ function PropertyInformationMultiFamilyBridge({
               id="checkboxes-tags-demo"
               options={top100Films}
               disableCloseOnSelect
-              getOptionLabel={(option) => option.title}
+              getOptionLabel={(option) => option.title} // ✅ No undefined error now
+              value={top100Films.filter((option) =>
+                formData.renovationDescript?.includes(option.title)
+              )} // ✅ Ensures only valid options are shown
+              onChange={(event, newValue) => {
+                setFormData({
+                  ...formData,
+                  renovationDescript: newValue.map((option) => option.title), // ✅ Store only titles
+                });
+              }}
               renderOption={(props, option, { selected }) => (
                 <li {...props}>
                   <Checkbox
@@ -451,30 +460,23 @@ function PropertyInformationMultiFamilyBridge({
                 </li>
               )}
               style={{
-                width: "100%", // Make sure the component takes full width
+                width: "100%",
                 backgroundColor: "white",
               }}
               renderInput={(params) => (
                 <TextField
                   {...params}
                   fullWidth
-                  multiline // Allows TextField to expand with more content
+                  multiline
                   error={!!fieldErrors.renovationDescript}
                   helperText={fieldErrors.renovationDescript}
-                  value={formData.renovationDescript || ""}
-                  onChange={(e) =>
-                    setFormData({
-                      ...formData,
-                      renovationDescript: e.target.value,
-                    })
-                  }
                   variant="outlined"
                   InputProps={{
                     ...params.InputProps,
                     style: {
                       minHeight: "56px",
-                      maxHeight: "150px", // Limit height to avoid excessive expansion
-                      overflowY: "auto", // Allows scrolling when height is exceeded
+                      maxHeight: "150px",
+                      overflowY: "auto",
                       backgroundColor: "white",
                     },
                   }}
@@ -482,7 +484,7 @@ function PropertyInformationMultiFamilyBridge({
               )}
               sx={{
                 "& .MuiAutocomplete-tag": {
-                  marginBottom: "5px", // Prevents tag overflow issues
+                  marginBottom: "5px",
                 },
               }}
             />
@@ -802,7 +804,7 @@ function PropertyInformationMultiFamilyBridge({
               </Typography>
             </Box>
           </Grid> */}
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             <Typography type="p" color="black">
               Please describe the renovation
             </Typography>
@@ -855,7 +857,7 @@ function PropertyInformationMultiFamilyBridge({
                 />
               )}
             />
-          </Grid>
+          </Grid> */}
 
           <Grid item xs={12} sm={12}>
             <FormControl fullWidth error={Boolean(fieldErrors?.asIsValue)}>
