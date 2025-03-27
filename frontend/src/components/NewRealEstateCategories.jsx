f9902df9b3cc1919855811f17662f74c8b234a5a
Jason Lui <luijason1221@gmail.com>
2025-03-27 10:39:41 -0700

    add "Ground Up" video in Real Estate Loans page

--- frontend/src/components/NewRealEstateCategories.jsx
+++ frontend/src/components/NewRealEstateCategories.jsx
@@ -687,7 +687,7 @@ const NewRealEstateCategories = () => {
                 <iframe
                   width="100%"
                   height="315"
-                  src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+                  src="https://www.youtube.com/embed/nGkGdLzN3Xw"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
