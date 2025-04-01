45bd384f97f087f05821dbc9e8a89c1b037aa9f3
Jason Lui <luijason1221@gmail.com>
2025-04-01 10:18:58 -0700

    add multifamily bridge video

--- frontend/src/components/NewRealEstateCategories.jsx
+++ frontend/src/components/NewRealEstateCategories.jsx
@@ -414,7 +414,7 @@ const NewRealEstateCategories = () => {
                 <iframe
                   width="100%"
                   height="315"
-                  src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
+                  src="https://www.youtube.com/embed/H0eBkcRky_k"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
